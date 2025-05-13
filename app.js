e107af3ff8865ac7b24540e57da915fefa763d58
jclewis33 <109756608+jclewis33@users.noreply.github.com>
2025-05-13 09:21:46 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+window.Webflow ||= [];
+window.Webflow.push(() => {
+  alert("hello world");
+});
+
+/*code to add into webflor project
+<script src="http://localhost:1234/app.js"></script>
+*/
